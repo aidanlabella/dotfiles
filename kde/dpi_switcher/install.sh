4abf4a6c1176a7df918747abfc6453f41cb8bd16
apl1341 <apl1341@rit.edu>
2020-09-02 22:33:06 -0400

    add arch zsh stuff

--- kde/dpi_switcher/install.sh
+++ kde/dpi_switcher/install.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/maldoinc/plasma-dpi-switcher.git ~/src
+#git clone https://github.com/maldoinc/plasma-dpi-switcher.git ~/src
 cd ~/src/plasma-dpi-switcher
 sudo python3 setup.py install --record files.txt
