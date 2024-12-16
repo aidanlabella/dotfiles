d8c68dcb2cb9cfbcd3e9d1cbd59b615d2c720831
aidanlabella <alabella@brown.edu>
2024-12-16 12:25:26 -0500

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,10 +8,27 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+config.tab_bar_at_bottom = true
 config.color_scheme = 'Dracula (Official)'
-config.font = wezterm.font('Menlo', { weight = 'Regular', italic = false })
+config.font = wezterm.font('CaskaydiaCove Nerd Font', { weight = 'Regular', italic = false })
 config.font_size = 13.0
 config.window_background_opacity = 0.95
+
+-- Vi-inspired tab switching :)
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'l',
+    mods = 'CTRL|ALT',
+    action = act.ActivateTabRelative(1),
+  },
+  {
+    key = 'h',
+    mods = 'CTRL|ALT',
+    action = act.ActivateTabRelative(-1),
+  },
+}
 
 -- and finally, return the configuration to wezterm
 return config
