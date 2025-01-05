37ee0e95dcbc3a99e118e0b8c0dc6c36e12d6d57
Aidan LaBella <alabella@brown.edu>
2025-01-05 12:02:57 -0500

    update theme code format

--- zsh/.aidans_theme.zsh
+++ zsh/.aidans_theme.zsh
@@ -17,59 +17,74 @@ RED="%{$(echo -e '\e[1;31m')%}"
 MAGENTA="%{$(echo -e '\e[1;35m')%}"
 RESET="%{$(echo -e '\e[0m')%}"
 
-# Git prompt status
+# Global variables for each prompt "widget"
 typeset -g ATH_GIT_PROMPT_STATUS=""
 typeset -g ATH_CONDA_PROMPT_STATUS=""
 typeset -g ATH_SLURM_PROMPT_STATUS=""
 
 get_git_sync_status() {
-  if git rev-parse --is-inside-work-tree &>/dev/null; then
-    local branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-    local upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+    if git rev-parse --is-inside-work-tree &>/dev/null; then
+        local branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+        local upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
 
-    local ahead=0
-    local behind=0
-    local modified=0
+        local ahead=0
+        local behind=0
+        local modified=0
 
-    if [[ -n $upstream ]]; then
-      ahead=$(git rev-list --count HEAD ^"$upstream" 2>/dev/null)
-      behind=$(git rev-list --count "$upstream" ^HEAD 2>/dev/null)
-    fi
+        if [[ -n $upstream ]]; then
+            ahead=$(git rev-list --count HEAD ^"$upstream" 2>/dev/null)
+            behind=$(git rev-list --count "$upstream" ^HEAD 2>/dev/null)
+        fi
 
-    modified=$(git status --porcelain | grep -c '^[ MADRCU]')
+        local to_push=""
+        local to_pull=""
 
-    if [[ -n $branch ]]; then
-      ATH_GIT_PROMPT_STATUS=" ${ORANGE} [${GREEN}$branch ${MAGENTA}$ahead ${CYAN}$behind ${YELLOW}✎$modified${ORANGE}]"
+        if [[ ahead -gt 0 ]]; then
+            to_push=" ${MAGENTA}$ahead"
+        fi
+
+        if [[ behind -gt 0 ]]; then
+            to_pull=" ${CYAN}$behind"
+        fi
+
+        modified=$(git status --porcelain | grep -c '^[ MADRCU]')
+
+        if [[ -n $branch ]]; then
+            ATH_GIT_PROMPT_STATUS=" ${ORANGE} [${GREEN} $branch${to_push}${to_pull} ${YELLOW}✎$modified${ORANGE}]"
+        else
+            ATH_GIT_PROMPT_STATUS=" ${ORANGE} [!(detached HEAD)]"
+        fi
     else
-      ATH_GIT_PROMPT_STATUS=" ${ORANGE} [!(detached HEAD)]"
+        ATH_GIT_PROMPT_STATUS=""
     fi
-  else
-    ATH_GIT_PROMPT_STATUS=""
-  fi
 }
 
-pyenv_prompt() {
-  if [[ -n $PYENV_VERSION ]]; then
-    echo " ($PYENV_VERSION) "
-  fi
-}
+#pyenv_prompt() {
+    #if [[ -n $PYENV_VERSION ]]; then
+        #echo " ($PYENV_VERSION) "
+    #fi
+#}
 
 conda_prompt() {
-  if [[ -n $CONDA_DEFAULT_ENV && $CONDA_DEFAULT_ENV != "base" ]]; then
-    ATH_CONDA_PROMPT_STATUS=" ($CONDA_DEFAULT_ENV) "
-  fi
+    if [[ -n $CONDA_DEFAULT_ENV && $CONDA_DEFAULT_ENV != "base" ]]; then
+        ATH_CONDA_PROMPT_STATUS=" ($CONDA_DEFAULT_ENV) "
+    else
+        ATH_CONDA_PROMPT_STATUS=""
+    fi
 }
 
 get_slurm_time_remaining() {
-  t="$(squeue -h -j $SLURM_JOBID -o %L)"
-  tf=$(echo "$t" | tr '-' 'd ')
-  echo "$tf"
+    t="$(squeue -h -j $SLURM_JOBID -o %L)"
+    tf=$(echo "$t" | tr '-' 'd ')
+    echo "$tf"
 }
 
 slurm_prompt() {
-  if [[ -n $SLURM_STEP_NODELIST ]]; then
-    ATH_SLURM_PROMPT_STATUS="$YELLOW $SLURM_STEP_NODELIST(T-$(get_slurm_time_remaining))"
-  fi
+    if [[ -n $SLURM_STEP_NODELIST ]]; then
+        ATH_SLURM_PROMPT_STATUS="$YELLOW $SLURM_STEP_NODELIST(T-$(get_slurm_time_remaining))"
+    else
+        ATH_SLURM_PROMPT_STATUS=""
+    fi
 }
 
 # Register precmd hooks
@@ -77,6 +92,6 @@ precmd_functions+=(get_git_sync_status conda_prompt slurm_prompt)
 
 setopt prompt_subst
 
-# Set prompt
+# Set prompt format strings
 PS1='${OS_COLOR}$OS_ICON [%~]${RESET}${ATH_GIT_PROMPT_STATUS}${GREEN}  ${RESET}'
 RPS1='${CYAN}${ATH_CONDA_PROMPT_STATUS}${RED}${ATH_SLURM_PROMPT_STATUS}${YELLOW}󰁥 $(date +"%I:%M:%S %p")${RESET}'
